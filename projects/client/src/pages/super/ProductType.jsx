cf5daf98619c1ffd88c7b34feeb0bb86308ab03b
Rizki <muhammadrizkiansyah60@gmail.com>
2023-06-26 11:36:34 +0700

    fix: typo diperbaiki

--- projects/client/src/pages/super/ProductType.jsx
+++ projects/client/src/pages/super/ProductType.jsx
@@ -1,12 +1,7 @@
 import Datas from "@/components/Datas";
-<<<<<<< HEAD
 import { useParams } from "react-router-dom";
 
 export default function ProductType () {
 	const { product_id } = useParams();
-=======
-
-export default function ProductType () {
 	
->>>>>>> origin/MW-7
 }
