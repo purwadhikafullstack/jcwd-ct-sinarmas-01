a1d368e94f26c7e7255f6e7e554a4fe9d61c9430
muhreezky <82578987+muhreezky@users.noreply.github.com>
2023-07-23 17:47:32 +0700

    fix (client): tabIndex -1 pada Carousel item

--- projects/client/src/components/CarouselItem.jsx
+++ projects/client/src/components/CarouselItem.jsx
@@ -1,6 +1,6 @@
 export default function CarouselItem (props) {
   return (
-    <div id={props.id} className="carousel-item flex justify-center items-center w-full">
+    <div tabIndex={-1} id={props.id} className="carousel-item flex justify-center items-center w-full">
       <img src={props.src} alt={props.alt} className={`${props.imgClass} max-w-[450px]`} />
     </div>
   )
