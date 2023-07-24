c08e6a90c1d22a4ec1a29db641818478e8b44082
muhreezky <82578987+muhreezky@users.noreply.github.com>
2023-07-24 21:26:49 +0700

    fix: compression dan fix verify dan reset pass

--- projects/client/src/pages/Home.jsx
+++ projects/client/src/pages/Home.jsx
@@ -2,11 +2,14 @@ import { getCategories } from "@/api/common";
 import CarouselItem from "@/components/CarouselItem";
 import Promotional from "@/components/Promotional";
 import { useEffect, useState } from "react";
-import { Hero } from "react-daisyui";
+import { Button, Hero } from "react-daisyui";
+import { useNavigate } from "react-router-dom";
+import { getRole } from "@/api/token";
 
 export default function Home() {
   const [data, setData] = useState({});
-
+  const navigate = useNavigate();
+  const link = getRole() || "login";
   useEffect(() => {
     (async () => {
       const categories = await getCategories(0);
@@ -15,19 +18,20 @@ export default function Home() {
   }, []);
   return (
     <>
-      <Hero className='mb-10'>
-        <Hero.Overlay className='bg-base-300' />
-        <Hero.Content className='text-center p-4'>
-          <div className='text-2xl'>Let's go shopping</div>
+      <Hero className="mb-10">
+        <Hero.Overlay className="bg-base-300" />
+        <Hero.Content className="text-center flex justify-center items-center flex-col gap-3 p-4">
+          <div className="text-2xl mb-6">Let's go shopping</div>
+          <Button onClick={() => navigate(link)}>
+            Start Shopping
+          </Button>
         </Hero.Content>
       </Hero>
       <Promotional />
-      <div className="text-3xl font-extrabold mt-4">
-        Categories
-      </div>
+      <div className="text-3xl font-extrabold mt-4">Categories</div>
       <div className="carousel my-6 bg-base-300 p-5 carousel-center">
         {data.rows?.map((val, key) => (
-          <CarouselItem text={val.category_name} key={key} />
+          <CarouselItem onClick={() => navigate(link)} text={val.category_name} key={key} />
         ))}
       </div>
     </>
